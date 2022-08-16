d77889f4baa69e66487f87792a09a4a36709bb6a
Evans Ochieng <evansochieng121@gmail.com>
2022-08-16 17:04:55 +0300

    Completed Phase1 Assignment

--- index.js
+++ index.js
@@ -1,40 +1,78 @@
-// Write your solution in this file!
+// // Phase 0
+// // Write your solution in this file!
+// const employee = {
+//     "name": "Evans",
+//     "streetAddress": "Kinoo"
+// };
+
+// // Update object (Undestructive)
+// function updateEmployeeWithKeyAndValue(employee, key, value){
+//     const newObject = {...employee}
+//     newObject[key] = value
+
+//     return newObject;
+//     // return {
+//     //     ...employee,
+//     //     [key]: value
+//     // }
+// };
+
+// // Destructively updating object
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+//     employee[key] = value
+//     return employee;
+// };
+
+// // Delete property from object
+// function deleteFromEmployeeByKey(employee, key){
+//     const newObj = { ...employee}
+
+//     delete newObj[key] 
+
+//     return newObj
+// }
+
+
+// // Destructively delete key from object
+// function destructivelyDeleteFromEmployeeByKey(employee, key){
+//     delete employee[key]
+
+//     return employee;
+// };
+
+
+// Phase 1
 const employee = {
-    "name": "Evans",
-    "streetAddress": "Kinoo"
+    'name': 'Evans',
+    'streetAdress': 'Kinoo',
 };
 
-// Update object (Undestructive)
-function updateEmployeeWithKeyAndValue(employee, key, value){
-    const newObject = {...employee}
-    newObject[key] = value
-
-    return newObject;
-    // return {
-    //     ...employee,
-    //     [key]: value
-    // }
+// updateEmployeeWithKeyAndValue()
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {
+        ...obj,
+        [key]: value
+    };
 };
 
-// Destructively updating object
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-    employee[key] = value
-    return employee;
+// destructivelyUpdateEmployeeWithKeyAndValue()
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
 };
 
-// Delete property from object
-function deleteFromEmployeeByKey(employee, key){
-    const newObj = { ...employee}
+// deleteFromEmployeeByKey()
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = {...obj};
 
-    delete newObj[key] 
+    delete newObj[key];
 
-    return newObj
+    return newObj;
+};
+
+// destructivelyDeleteFromEmployeeByKey()
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+
+    return obj;
 }
-
-
-// Destructively delete key from object
-function destructivelyDeleteFromEmployeeByKey(employee, key){
-    delete employee[key]
-
-    return employee;
-};
